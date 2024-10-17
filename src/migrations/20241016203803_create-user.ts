15736e04151b4331803a2b815863273db2678a7d
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2024-10-17 10:36:54 -0600

    fix(Migrations): missing not nullable constraint

--- src/migrations/20241016203803_create-user.ts
+++ src/migrations/20241016203803_create-user.ts
@@ -9,7 +9,7 @@ export async function up (knex: Knex): Promise<void> {
         indexName: 'Users_user_unique'
       }).notNullable()
       table.string('password').notNullable()
-      table.specificType('permissions', 'text[]')
+      table.specificType('permissions', 'text[]').notNullable()
       table.timestamp('createdAt').notNullable()
       table.timestamp('updatedAt').notNullable()
     })
