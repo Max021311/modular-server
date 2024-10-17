46487a52a8f9111b8dfec0db094addd90857ec4e
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2024-10-17 10:21:12 -0600

    feat(Knex): add migrations

---
+++ src/migrations/20241016203803_create-user.ts
@@ -0,0 +1,37 @@
+import type { Knex } from 'knex'
+
+export async function up (knex: Knex): Promise<void> {
+  return knex.schema
+    .createTable('Users', function (table) {
+      table.increments('id').primary().notNullable()
+      table.string('name').notNullable()
+      table.string('user').unique({
+        indexName: 'Users_user_unique'
+      }).notNullable()
+      table.string('password').notNullable()
+      table.specificType('permissions', 'text[]')
+      table.timestamp('createdAt').notNullable()
+      table.timestamp('updatedAt').notNullable()
+    })
+    .raw(`
+    CREATE OR REPLACE FUNCTION preserve_created_at_update()
+    RETURNS TRIGGER AS $$
+    BEGIN
+      NEW."createdAt" = OLD."createdAt";
+      RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+    `)
+    .raw(`
+    CREATE TRIGGER preserve_created_at_trigger
+    BEFORE UPDATE ON "Users"
+    FOR EACH ROW
+    EXECUTE FUNCTION preserve_created_at_update();
+    `)
+}
+
+export async function down (knex: Knex): Promise<void> {
+  return knex.schema
+    .raw('DROP FUNCTION IF EXISTS preserve_created_at_update();')
+    .raw('DROP TRIGGER IF EXISTS preserve_created_at_trigger ON "Users";')
+}
