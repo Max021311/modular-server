903584ae61cd981d5f18d4474fb0895d270176c9
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2025-09-08 21:14:40 -0600

    feat(Student API): add endpoint to authenticate as student

--- src/routes/students/index.ts
+++ src/routes/students/index.ts
@@ -103,6 +103,20 @@ const routesPlugin: FastifyPluginAsync = async function routesPlugin (fastify) {
     }
   } as const satisfies JSONSchema
 
+  const loginBodySchema = {
+    type: 'object',
+    properties: {
+      email: {
+        type: 'string',
+        format: 'email'
+      },
+      password: {
+        type: 'string'
+      }
+    },
+    required: ['email', 'password']
+  } as const satisfies JSONSchema
+
   const updateStudentBodySchema = {
     type: 'object',
     properties: {
@@ -125,6 +139,30 @@ const routesPlugin: FastifyPluginAsync = async function routesPlugin (fastify) {
     },
     additionalProperties: false
   } as const satisfies JSONSchema
+
+  server.route({
+    method: 'POST',
+    url: '/auth',
+    schema: {
+      description: 'Allow log in as an student user',
+      tags: ['Students'],
+      body: loginBodySchema,
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            token: { type: 'string' }
+          },
+          required: ['token'],
+          additionalProperties: false
+        } as const satisfies JSONSchema
+      }
+    },
+    async handler (request, reply) {
+      const token = await request.server.services.studentService().login(request.body.email, request.body.password)
+      await reply.status(200).send({ token })
+    }
+  })
 
   server.route({
     method: 'POST',
