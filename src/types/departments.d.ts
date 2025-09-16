1bc4a5fe00340bfc4f9734a3e85b2af4d6ed6b21
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2025-09-16 01:00:28 -0600

    feat(Types): add department types

---
+++ src/types/departments.d.ts
@@ -0,0 +1,14 @@
+export interface Department {
+  id: number
+  name: string
+  address: string
+  phone: string
+  email: string
+  chiefName: string,
+  createdAt: Date
+  updatedAt: Date
+  searchVector: string
+}
+
+export type CreateDepartment = Omit<Department, 'id'|'searchVector'>
+export type UpdateDepartment = Partial<Omit<Department, 'id'|'createdAt'|'searchVector'>>
