f5906e8f1a17ea487b6be52ec4926f83b391df19
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2025-09-16 01:02:02 -0600

    feat(Migrations): create department table

---
+++ src/migrations/20250916054320_create-department.ts
@@ -0,0 +1,37 @@
+import type { Knex } from 'knex'
+import configuration from '#src/common/configuration'
+
+export async function up (knex: Knex): Promise<void> {
+  await knex.schema
+    .createTable('Departments', function (table) {
+      table.increments('id').primary().notNullable()
+      table.string('name').notNullable()
+      table.string('address').notNullable()
+      table.string('phone').notNullable()
+      table.string('email').notNullable()
+      table.string('chiefName').notNullable()
+      table.timestamp('createdAt').notNullable().index()
+      table.timestamp('updatedAt').notNullable()
+    })
+  await knex.raw(`
+    ALTER TABLE "Departments" 
+    ADD COLUMN search_vector tsvector 
+    GENERATED ALWAYS AS (
+      to_tsvector('${configuration.textSearch.language}', 
+        COALESCE(name, '') || ' ' || 
+        COALESCE(phone, '') || ' ' || 
+        COALESCE(email, '') || ' ' || 
+        COALESCE("chiefName", '')
+      )
+    ) STORED
+  `)
+
+  await knex.raw(`
+    CREATE INDEX idx_departments_search ON "Departments" USING GIN(search_vector)
+  `)
+}
+
+export async function down (knex: Knex): Promise<void> {
+  return knex.schema
+    .dropTable('Departments')
+}
