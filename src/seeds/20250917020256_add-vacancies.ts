bcc801e1ba94d4a2e86b8744b9283bed6a31e550
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2025-09-17 16:35:54 -0600

    feat(Seeds): update seeds for vacancies

--- src/seeds/20250917020256_add-vacancies.ts
+++ src/seeds/20250917020256_add-vacancies.ts
@@ -6,7 +6,7 @@ export async function seed (knex: Knex): Promise<void> {
 
   await knex('Vacancies').insert([{
     name: 'Soporte técnico',
-    description: '',
+    description: faker.lorem.paragraphs({ min: 2, max: 5 }),
     slots: 10,
     cycleId: 5,
     departmentId: faker.number.int({ min: 1, max: 50 }),
