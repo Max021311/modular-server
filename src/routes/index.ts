4bd6dadd0c7c8a064aa6affb2ab3150fab9666ab
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2025-09-26 21:53:49 -0600

    feat(API): register routes plugins

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,13 +6,20 @@ import departmentsRoutesPlugin from './departments/index.js'
 import cyclesRoutesPlugin from './cycles/index.js'
 import vacanciesRoutesPlugin from './vacancies/index.js'
 import comissionOfficesRoutesPlugin from './comission-offices/index.js'
+import finalReportsRoutesPlugin from './final-reports/index.js'
+import reportsRoutesPlugin from './reports/index.js'
+import apiPlugin from './api/index.js'
 
 export default fp(async function RoutesPlugin (fastify) {
-  fastify.register(userRoutesPlugin, { prefix: '/user' })
-  fastify.register(studentsRoutesPlugin, { prefix: '/students' })
-  fastify.register(careersRoutesPlugin, { prefix: '/careers' })
-  fastify.register(departmentsRoutesPlugin, { prefix: '/departments' })
-  fastify.register(cyclesRoutesPlugin, { prefix: '/cycles' })
-  fastify.register(vacanciesRoutesPlugin, { prefix: '/vacancies' })
-  fastify.register(comissionOfficesRoutesPlugin, { prefix: 'comission-offices' })
+  fastify
+    .register(userRoutesPlugin, { prefix: '/user' })
+    .register(studentsRoutesPlugin, { prefix: '/students' })
+    .register(careersRoutesPlugin, { prefix: '/careers' })
+    .register(departmentsRoutesPlugin, { prefix: '/departments' })
+    .register(cyclesRoutesPlugin, { prefix: '/cycles' })
+    .register(vacanciesRoutesPlugin, { prefix: '/vacancies' })
+    .register(comissionOfficesRoutesPlugin, { prefix: '/comission-offices' })
+    .register(finalReportsRoutesPlugin, { prefix: '/final-reports' })
+    .register(reportsRoutesPlugin, { prefix: '/reports' })
+    .register(apiPlugin, { prefix: '/api' })
 })
