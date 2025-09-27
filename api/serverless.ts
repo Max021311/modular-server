f4423df79b46444416455e3ffd236b7006c62c1b
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2025-09-26 22:52:30 -0600

    feat(Vercel): add serveless module

---
+++ api/serverless.ts
@@ -0,0 +1,9 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import build from '../src/build.js'
+
+const server = build()
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  await server.ready()
+  server.server.emit('request', req, res)
+}
