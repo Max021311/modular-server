fbf046f3669018317d62dafacfe1cac7b9f6c369
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2025-09-29 11:47:10 -0600

    feat(Template service): add `report-status-change`, `final-report-status-change` and `comission-office-status-change`

--- src/service/template-render/types.d.ts
+++ src/service/template-render/types.d.ts
@@ -36,10 +36,46 @@ export interface RecoverUserPassword extends TemplateDefinition {
   }
 }
 
+export interface ReportStatusChange extends TemplateDefinition {
+  template: 'report-status-change'
+  file: 'body.html' | 'body.txt' | 'subject.txt'
+  data: {
+    studentName: string
+    vacancyName: string
+    cycleName: string
+    status: 'aprobado' | 'rechazado'
+  }
+}
+
+export interface FinalReportStatusChange extends TemplateDefinition {
+  template: 'final-report-status-change'
+  file: 'body.html' | 'body.txt' | 'subject.txt'
+  data: {
+    studentName: string
+    vacancyName: string
+    cycleName: string
+    status: 'aprobado' | 'rechazado'
+  }
+}
+
+export interface ComissionOfficeStatusChange extends TemplateDefinition {
+  template: 'comission-office-status-change'
+  file: 'body.html' | 'body.txt' | 'subject.txt'
+  data: {
+    studentName: string
+    vacancyName: string
+    cycleName: string
+    status: 'aprobado' | 'rechazado'
+  }
+}
+
 type RenderParams = InviteStudent
 | InviteUser
 | RecoverStudentPassword
 | RecoverUserPassword
+| ReportStatusChange
+| FinalReportStatusChange
+| ComissionOfficeStatusChange
 
 export interface TemplateRenderI {
   render(params: RenderParams): string
